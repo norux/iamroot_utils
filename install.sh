8635ba79a2267805a8af6746703b41b31a4d48d0
Andrew Heebum Kwak <kh-1143@hanmail.net>
2015-08-30 21:43:09 +0900

    add vimrc & advance install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,10 @@ mkdir -p $PLUGIN_PATH
 cp $BASEDIR/plugin/iamroot_comment.vim $PLUGIN_PATH
 
 
-# for key mapping (add lines to ~/.vimrc)
-echo "map <F5> <esc>:call IC_AnalyzingCode() <cr>" >> $HOME/.vimrc
-echo "map <F6> <esc>:call IC_DailySummary() <cr>" >> $HOME/.vimrc
-echo "map <F7> <esc>:call IC_FileSummary() <cr>" >> $HOME/.vimrc
+# 기존의 vimrc 파일을 백업한다.
+mkdir -p backup
+DATE=`date +%Y%m%d_%H%M%S`
+cp ~/.vimrc backup/vimrc.back.$DATE
+
+# copy .vimrc to ~/.vimrc
+cp vimrc ~/.vimrc
